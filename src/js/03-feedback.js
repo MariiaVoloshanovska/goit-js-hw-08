6d84dcb837d6a8df0cf1685770fa31288a39146c
MariiaVoloshanovska <masterskaiamary@gmail.com>
2023-05-27 15:55:04 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,41 +1,3 @@
-// import throttle from 'lodash.throttle';
-
-// const form = document.querySelector('.feedback-form');
-// const emailInput = form.querySelector('input[name="email"]');
-// const messageInput = form.querySelector('textarea[name="message"]');
-// const STORAGE_KEY = 'feedback-form-state';
-
-// const storageFormTotal = throttle(() => {
-//   const feedbackForm = {
-//     email: emailInput.value,
-//     message: messageInput.value,
-//   };
-//   localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackForm));
-// }, 500);
-
-// form.addEventListener('input', storageFormTotal);
-// const feedbackForm = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
-
-// //  Збереженні значення
-// emailInput.value = feedbackForm.email || '';
-// messageInput.value = feedbackForm.message || '';
-
-// form.addEventListener('submit', formSubmit);
-
-// function formSubmit(e) {
-//   e.preventDefault();
-
-//   const forFeedback = {
-//     email: emailInput.value,
-//     message: messageInput.value,
-//   };
-//   console.log(forFeedback);
-//   // Очищистити локальне сховище & поля форми
-//   localStorage.removeItem(STORAGE_KEY);
-//   emailInput.value = '';
-//   messageInput.value = '';
-// }
-
 import throttle from 'lodash.throttle';
 
 const refs = {
@@ -81,3 +43,41 @@ function populateFormInput() {
 }
 refs.formEL.addEventListener('input', throttle(onFormInput, 500));
 refs.formEL.addEventListener('submit', onFormButSubmit);
+
+// import throttle from 'lodash.throttle';
+
+// const form = document.querySelector('.feedback-form');
+// const emailInput = form.querySelector('input[name="email"]');
+// const messageInput = form.querySelector('textarea[name="message"]');
+// const STORAGE_KEY = 'feedback-form-state';
+
+// const storageFormTotal = throttle(() => {
+//   const feedbackForm = {
+//     email: emailInput.value,
+//     message: messageInput.value,
+//   };
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackForm));
+// }, 500);
+
+// form.addEventListener('input', storageFormTotal);
+// const feedbackForm = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+// //  Збереженні значення
+// emailInput.value = feedbackForm.email || '';
+// messageInput.value = feedbackForm.message || '';
+
+// form.addEventListener('submit', formSubmit);
+
+// function formSubmit(e) {
+//   e.preventDefault();
+
+//   const forFeedback = {
+//     email: emailInput.value,
+//     message: messageInput.value,
+//   };
+//   console.log(forFeedback);
+//   // Очищистити локальне сховище & поля форми
+//   localStorage.removeItem(STORAGE_KEY);
+//   emailInput.value = '';
+//   messageInput.value = '';
+// }
