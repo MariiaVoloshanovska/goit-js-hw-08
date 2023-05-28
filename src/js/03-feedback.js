f99d0613f25d5d7d653b23f10edc636fa798b3ee
MariiaVoloshanovska <masterskaiamary@gmail.com>
2023-05-28 08:50:59 +0300

    3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,82 +1,46 @@
-// import throttle from 'lodash.throttle';
-
-// const refs = {
-//   formEL: document.querySelector('.feedback-form'),
-// };
-// const STORAGE_KEY = 'feedback-form-state';
-// formInput();
-// //+ inform to localStorage
-// let allUserInfo = {};
-
-// function onFormInput(e) {
-//   let form = e.target;
-//   allUserInfo[form.name] = form.value;
-//   localStorage.setItem(STORAGE_KEY, JSON.stringify(allUserInfo));
-// }
-
-// //parse our localStorage and save clients info in our input and texterea
-// function formInput() {
-//   try {
-//     let formElements = refs.formEL.elements;
-//     const savedUserInfo = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-//     if (savedUserInfo) {
-//       formElements.email.value = savedUserInfo.email || '';
-//       formElements.message.value = savedUserInfo.message || '';
-//     }
-//     // for (const field in savedUserInfo) {
-//     //     formElements[field].value = savedUserInfo[field] || '';
-//     // }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// //clean input and texterea after click on button Submit
-// function onFormButSubmit(e) {
-//   e.preventDefault();
-
-//   e.currentTarget.reset();
-//   localStorage.removeItem(STORAGE_KEY);
-// }
-
-// refs.formEL.addEventListener('input', throttle(onFormInput, 500));
-// refs.formEL.addEventListener('submit', onFormButSubmit);
-
+// Імпортуємо функцію `throttle` з пакету `lodash.throttle`
 import throttle from 'lodash.throttle';
 
+// Отримуємо посилання на елементи DOM форми
 const form = document.querySelector('.feedback-form');
-const emailInput = form.querySelector('input[name="email"]');
-const messageInput = form.querySelector('textarea[name="message"]');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+
+// Ключ для зберігання даних у `localStorage`
 const STORAGE_KEY = 'feedback-form-state';
 
-const storageFormTotal = throttle(() => {
-  const feedbackForm = {
-    email: emailInput.value,
-    message: messageInput.value,
-  };
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackForm));
-}, 500);
+// Отримуємо збережені дані форми з `localStorage`
+const formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-form.addEventListener('input', storageFormTotal);
-const feedbackForm = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+// Додаємо обробник події `input` до форми, використовуючи `throttle` для обмеження частоти виклику функції `onFormInput`
+form.addEventListener('input', throttle(onFormInput, 500));
 
-//  Збереженні значення
-emailInput.value = feedbackForm.email || '';
-messageInput.value = feedbackForm.message || '';
+// Додаємо обробник події `submit` до форми
+form.addEventListener('submit', onFormSubmit);
 
-form.addEventListener('submit', formSubmit);
-
-function formSubmit(e) {
-  e.preventDefault();
-
-  const forFeedback = {
-    email: emailInput.value,
-    message: messageInput.value,
-  };
-  console.log(forFeedback);
-  // Очищистити локальне сховище & поля форми
-  localStorage.removeItem(STORAGE_KEY);
-  emailInput.value = '';
-  messageInput.value = '';
+// Функція, яка викликається при введенні даних у форму
+function onFormInput() {
+  // Зберігаємо значення полів форми у `localStorage`
+  localStorage.setItem(
+    STORAGE_KEY,
+    JSON.stringify({ email: email.value, message: message.value })
+  );
 }
+
+// Функція, яка викликається при поданні форми
+function onFormSubmit(evt) {
+  evt.preventDefault(); // Зупиняємо стандартну поведінку форми (у цьому випадку, перезавантаження сторінки)
+  evt.currentTarget.reset(); // Очищуємо форму
+  localStorage.removeItem(STORAGE_KEY); // Видаляємо збережені дані з `localStorage`
+}
+
+// Функція, яка заповнює поля форми збереженими значеннями з `localStorage`
+function getValueFromLocalStorage() {
+  if (formData === null) {
+    return; // Якщо збережених даних немає, виходимо з функції
+  }
+  email.value = formData.email; // Заповнюємо поле форми збереженим значенням електронної пошти
+  message.value = formData.message; // Заповнюємо поле форми збереженим значенням повідомлення
+}
+
+getValueFromLocalStorage(); // Викликаємо функцію для заповнення полів форми зі збереженими значеннями з `localStorage`
